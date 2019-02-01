a33749009feb6d3cbadde7d17171986ad43e2c5e
Sqall <arcangel6191@gmail.com>
2019-02-01 12:03:45 -0300

    rolling back

--- api/routes/clothes.js
+++ api/routes/clothes.js
@@ -12,7 +12,7 @@ router.get('/',(req,res,next) => {
         .exec()
         .then(doc => {
             if (doc){
-                res.status(200).json(doc[0]);
+                res.status(200).json(doc);
             } else{
                 res.status(404).json({message: 'No valid entry found'});
             }
