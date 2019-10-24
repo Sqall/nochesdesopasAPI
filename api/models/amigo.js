231c682bd89c055748577674b74ca31655904f6a
Sqall <arcangel6191@gmail.com>
2019-10-24 12:08:48 -0300

    Modificado modelo para tener mas datos sobre la persona: situacion, familia, datos personales

--- api/models/amigo.js
+++ api/models/amigo.js
@@ -4,15 +4,32 @@ const AmigoSchema = mongoose.Schema({
 	id:{
 		type: Number,
 		index:true
-	},
-	name: String,
-    notes: [{fecha: Date, text: String}],    
-    team: Number,
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9!](?:[a-z0-9-]*[a-z09])?\.)+[a-z0-9](?:[a-z0-9]*[a-z0-9])?/}
+  },
+  // Datos Primarios
+  name: String,
+  surname: String,
+  age: Number,
+  location: String,
+  ds_1: String, // Situacion actual    
+  // Datos Secundarios
+  ds_2: String, // Como llego a su situacion
+  dni: Number, // tamaño fijo?
+  d_fam: String, // Familia a cargo
+  t_st: String, // Tiempo en calle
+  d_work: String, // datos sobre su trabajo o no
+  d_gob: String, //datos sobre ayuda gubernamental
+  d_health: String, // datos sobre salud
+  d_hostel: String, // datos sobre parador 
+  formalities: [String], // Tramites
+  d_sizes: [], // Arreglo Vacio, se puede insertar cualquier data
+  search: [], // Arreglo Vacio, se puede insertar cualquier data
+  notes: [{fecha: Date, text: String}],
+  team: Number,
+  email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9!](?:[a-z0-9-]*[a-z09])?\.)+[a-z0-9](?:[a-z0-9]*[a-z0-9])?/}
 });
 
 module.exports = mongoose.model('amigos',AmigoSchema);
